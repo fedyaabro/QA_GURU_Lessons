99a2d0cff1ac91dbc86e8152535f7ca2ef23e2e6
Fedor Abrosimov <124066164+FedorAbrosimov@users.noreply.github.com>
2023-12-19 21:08:41 +0300

    Первое задание

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -10,7 +10,6 @@ public class SearchTests {
     void successfulSearchTest() {
     
         Configuration.pageLoadStrategy = "eager";
-     
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
