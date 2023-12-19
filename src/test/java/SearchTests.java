31cbefd12808d778f922f10601772d2a553f6306
Fedor Abrosimov <124066164+FedorAbrosimov@users.noreply.github.com>
2023-12-19 21:05:30 +0300

    Первое задание

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -10,10 +10,9 @@ public class SearchTests {
     void successfulSearchTest() {
     
         Configuration.pageLoadStrategy = "eager";
-    
+     
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
-    
 }
